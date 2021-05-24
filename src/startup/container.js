8eb9e0729a26b5cdbc4ad562bd32045a91c8a435
rodrigo <8kdots@gmail.com>
2021-05-24 15:34:02 -0500

    [ADD] mongoose-autopopualte dependecy add

--- src/startup/container.js
+++ src/startup/container.js
@@ -15,6 +15,9 @@ const { HomeController } = require('../controllers');
 const { HomeRoutes } = require('../routes/index.routes')
 const Routes = require('../routes')
 
+//models
+const { User, Comment, Idea } = require('../models')
+
 const container = createContainer();
 
 container
@@ -31,6 +34,11 @@ container
 })
 .register({
     HomeRoutes: asFunction(HomeRoutes).singleton()
+})
+.register({
+    User: asValue(User),
+    Idea: asValue(Idea),
+    Comment: asValue(Comment)
 })
 
 module.exports = container;
