b22b239c79232af924160e898c45f17fc2ebe15d
rodrigo <8kdots@gmail.com>
2021-05-25 13:17:59 -0500

    [ADD] add repositories

--- src/startup/container.js
+++ src/startup/container.js
@@ -18,6 +18,15 @@ const Routes = require('../routes')
 //models
 const { User, Comment, Idea } = require('../models')
 
+//repositories
+const { 
+    UserRepository,
+    IdeaRepository,
+    CommentRepository    
+} = require('../repositories');
+
+
+
 const container = createContainer();
 
 container
@@ -39,6 +48,11 @@ container
     User: asValue(User),
     Idea: asValue(Idea),
     Comment: asValue(Comment)
+})
+.register({
+    UserRepository: asClass(UserRepository).singleton(),
+    IdeaRepository: asClass(IdeaRepository).singleton(),
+    CommentRepository: asClass(CommentRepository).singleton(),
 })
 
 module.exports = container;
